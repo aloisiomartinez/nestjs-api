705f43c3bb21f1fc5a7cd0cbd46487e70411560b
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-12-08 16:57:28 -0300

    backoffice creating controller

--- src/backoffce/controllers/custumer.controller.ts
+++ src/backoffce/controllers/custumer.controller.ts
@@ -1,19 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
-@Controller()
+@Controller('v1/customers')
 export class CustomerController {
+  @Get()
   get() {
     return 'Obter os clients';
   }
 
+  @Post()
   post() {
     return 'Criar os clients';
   }
 
+  @Put()
   put() {
     return 'Atualizar os clients';
   }
 
+  @Delete()
   delete() {
     return 'Remover os clients';
   }
