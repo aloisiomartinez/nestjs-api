491f1c3523a1497ff9f47b1d27e6c98559b5b329
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-12-09 17:42:25 -0300

    customer contract

---
+++ src/backoffce/contracts/customer.contracts.ts
@@ -0,0 +1,18 @@
+import { Flunt } from 'src/utils/flunt';
+import { Custumer } from '../models/custumer.model';
+import { Contract } from './contract';
+
+export class CreateCustumerContract implements Contract {
+  errors: any[];
+  validate(model: Custumer): boolean {
+    const flunt = new Flunt();
+
+    flunt.hasMinLen(model.name, 5, 'Nome inválido');
+    flunt.isEmail(model.email, 'E-mail inválido');
+    flunt.isFixedLen(model.document, 11, 'CPF inválido');
+    flunt.hasMinLen(model.password, 6, 'Senha inválida');
+
+    this.errors = flunt.errors;
+    return flunt.isValid();
+  }
+}
