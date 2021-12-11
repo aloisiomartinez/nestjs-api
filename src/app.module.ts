2d0e2289b651944a4336c6389bff7804fc1676e5
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-12-11 20:29:50 -0300

    mongodb connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BackoffceModule } from './backoffce/backoffce.module';
 
 @Module({
-  imports: [BackoffceModule],
+  imports: [MongooseModule.forRoot('CONNECTION_STRING'), BackoffceModule],
   controllers: [],
   providers: [],
 })
--- src/backoffce/backoffce.module.ts
+++ src/backoffce/backoffce.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerController } from './controllers/custumer.controller';
+import { CustomerSchema } from './schemas/customer.schema';
+import { UserSchema } from './schemas/user.shcma';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Customer',
+        schema: CustomerSchema,
+      },
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [CustomerController],
 })
 export class BackoffceModule {}
