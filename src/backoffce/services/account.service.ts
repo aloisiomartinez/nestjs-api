e4b4d9c48450c793706cdd33abe4163c76f7a157
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-12-16 16:50:57 -0300

    createUser service

---
+++ src/backoffce/services/account.service.ts
@@ -0,0 +1,14 @@
+import { User } from './../models/user.model';
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class AccountService {
+  constructor(@InjectModel('User') private readonly model: Model<User>) {}
+
+  create(data: User): Promise<User> {
+    const user = new this.model(data);
+    return user.save();
+  }
+}
