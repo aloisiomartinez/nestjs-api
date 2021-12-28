ce763ec47fa6528a848b97b8c758de457050a006
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-12-28 16:56:04 -0300

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackoffceModule } from './modules/backoffce/backoffce.module';
 import { StoreModule } from 'src/modules/store/store.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://admin:admin@nestpetshoapi.nggxu.mongodb.net/test',
     ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'mysql',
+      database: '7180',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
     BackoffceModule,
     StoreModule,
   ],
