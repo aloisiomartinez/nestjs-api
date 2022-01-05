e389cd4c335cc18e85ec23996c33b0c44e7e1e03
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-01-04 22:06:57 -0300

    postman file

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     ),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'mysql',
       database: '7180',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true
